a613e60fd6cb77b6e1ffa6c337da99177ef08204
Klajd Deda <kdeda@id-design.com>
2025-12-04 00:50:53 -0500

    * Updated dependencies.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,17 +15,19 @@ let package = Package(
         )
     ],
     dependencies: [
+        .package(url: "https://github.com/kdeda/idd-log4-swift.git", "2.2.6" ..< "3.0.0"),
         // .package(name: "idd-alert", path: "../idd-alert"),
-        .package(url: "https://github.com/kdeda/idd-alert.git", "2.1.1" ..< "3.0.0"),
+        .package(url: "https://github.com/kdeda/idd-alert.git", "2.1.3" ..< "3.0.0"),
         // .package(name: "idd-swift", path: "../idd-swift"),
         // .package(url: "https://github.com/kdeda/idd-swift.git", "2.3.6" ..< "3.0.0"),
-        .package(url: "https://github.com/kdeda/idd-swiftui.git", "2.1.2" ..< "3.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.17.0")
+        .package(url: "https://github.com/kdeda/idd-swiftui.git", "2.2.3" ..< "3.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", from: "1.23.1")
     ],
     targets: [
         .target(
             name: "IDDSoftwareUpdate",
             dependencies: [
+                .product(name: "Log4swift", package: "idd-log4-swift"),
                 .product(name: "IDDAlert", package: "idd-alert"),
                 .product(name: "IDDSwiftUI", package: "idd-swiftui"),
                 .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
